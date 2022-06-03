a06a51e9cf2e6b2bd94e0e538f4f1f2a6e11abb0
DESKTOP-TL1SABH\ZDP <zhangpeikkk@163.com>
2022-06-03 15:11:50 +0800

    项目初始化

---
+++ src/views/fileShow/index.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="fileShow">
+    <el-row>
+      <el-col :span="12">
+        <div class="pdf">
+          <pdf
+            :src="pdfSrc"
+            v-for="item in numPages"
+            :key="item"
+            :page="item"
+          ></pdf>
+        </div>
+      </el-col>
+      <el-col :span="12">
+        <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
+          <el-tab-pane label="文档修正" name="first">
+            <el-form ref="form" :model="form" label-width="120px" class="form">
+              <el-form-item label="关键字：">
+                <el-input v-model="form.name"></el-input>
+              </el-form-item>
+              <el-form-item label="修正：">
+                <el-input v-model="form.region"></el-input>
+              </el-form-item>
+              <div class="buttonGrunp">
+                <el-button type="primary" @click="onSubmit">保存</el-button>
+              </div>
+            </el-form>
+          </el-tab-pane>
+          <el-tab-pane label="新增规则" name="second">
+            <el-form ref="form" :model="form" label-width="120px" class="form">
+              <el-form-item label="关键字：">
+                <el-input v-model="form.name"></el-input>
+              </el-form-item>
+              <el-form-item label="业务规则模板：">
+                <el-input v-model="form.region"></el-input>
+              </el-form-item>
+              <div class="buttonGrunp">
+                <el-button type="primary" @click="save">保存</el-button>
+              </div>
+            </el-form>
+          </el-tab-pane>
+        </el-tabs>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import pdf from "vue-pdf";
+export default {
+  name: "fileShow",
+  components: {
+    pdf,
+  },
+  data() {
+    return {
+      pdfSrc: "/static/aaa.pdf",
+      numPages: "", //  pdf 文件总页数
+      activeName: "first",
+      form: {
+        name: "",
+        region: "",
+      },
+    };
+  },
+  mounted() {
+    this.getNumPages(this.pdfSrc);
+  },
+  methods: {
+    getNumPages(url) {
+      var loadingTask = pdf.createLoadingTask(url);
+      loadingTask.promise
+        .then((pdf) => {
+          debugger;
+          this.pdfSrc = loadingTask;
+          this.numPages = pdf.numPages;
+        })
+        .catch((err) => {
+          console.error("pdf加载失败");
+        });
+    },
+    handleClick(tab, event) {
+      console.log(tab, event);
+    },
+    onSubmit() {
+      console.log("submit!");
+    },
+    save() {
+      const loading = this.$loading({
+        lock: true,
+        text: "Loading",
+        spinner: "el-icon-loading",
+        background: "rgba(0, 0, 0, 0.7)",
+      });
+      setTimeout(() => {
+        this.getNumPages("/static/bbb.pdf");
+        loading.close();
+      }, 2000);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.fileShow {
+  .pdf {
+    height: calc(100vh - 50px);
+    overflow: auto;
+  }
+  .form {
+    padding-top: 50px;
+    width: 80%;
+    margin: 0 auto;
+    .buttonGrunp {
+      text-align: center;
+    }
+  }
+}
+</style>
